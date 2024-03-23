ccef4cf469802facb3f7385865b3dfaea4509977
dinhanhquoc <anhquoc004@gmail.com>
2024-03-23 15:41:19 +0700

    Create Var ObjectView
    Create Var ObjectView To Control

--- Cau2_AppBMI/app/src/main/java/ntu/MSSV63131112/MainActivity.java
+++ Cau2_AppBMI/app/src/main/java/ntu/MSSV63131112/MainActivity.java
@@ -1,6 +1,12 @@
 package ntu.MSSV63131112;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +14,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    EditText edt_hovaten;
+    EditText edt_cc;
+    EditText edt_cn;
+    TextView tv_mota;
+    Button btn_tinhtoan;
+    ListView lv_history;
+    ArrayAdapter<String> boNguon;
+    ArrayList<String> list;
+
+    RadioButton rbtn_nam;
+    RadioButton rbtn_nu;
+    RadioButton rbtn_khac;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
+
 }
