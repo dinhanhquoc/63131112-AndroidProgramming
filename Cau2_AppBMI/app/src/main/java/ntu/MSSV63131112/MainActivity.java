910462d6d64283f414a2c631c2a7a7d7fa897263
dinhanhquoc <anhquoc004@gmail.com>
2024-03-23 16:30:32 +0700

    Fix Function
    Fix Function

--- Cau2_AppBMI/app/src/main/java/ntu/MSSV63131112/MainActivity.java
+++ Cau2_AppBMI/app/src/main/java/ntu/MSSV63131112/MainActivity.java
@@ -15,6 +15,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -54,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
         rbtn_nam = findViewById(R.id.rbtn_nam);
         rbtn_nu = findViewById(R.id.rbtn_nu);
         rbtn_khac = findViewById(R.id.rbtn_khac);
+        btn_tinhtoan = findViewById(R.id.btn_tinhtoan);
         list = new ArrayList<>();
     }
 
@@ -67,9 +69,10 @@ public class MainActivity extends AppCompatActivity {
         return cn/Math.pow(cc,2);
     }
     private void CompareBMI(double bmi){
-
+        DecimalFormat df= new DecimalFormat("#.#");
         if(bmi < 18.5){
             double cnyeucau =  18.5 * Math.pow(cc, 2) - cn;
+            cnyeucau = Double.parseDouble(df.format(cnyeucau));
             String yeucau ="Bạn gầy hơn so với quy định cần tăng "+cnyeucau+" Kg";
             tv_mota.setText(yeucau);
             return;
@@ -80,12 +83,15 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
         if(bmi>=25&& bmi<30){
+
             double cnyeucau = cn - 24.9 * Math.pow(cc, 2);
+            cnyeucau = Double.parseDouble(df.format(cnyeucau));
             String yeucau ="Bạn bị thừa cân cần giảm "+cnyeucau+" Kg";
             tv_mota.setText(yeucau);
             return;
         }
         double cnyeucau = cn - 24.9 * Math.pow(cc, 2);
+        cnyeucau = Double.parseDouble(df.format(cnyeucau));
         String yeucau ="Bạn bị béo phì cần giảm "+cnyeucau+" Kg";
         tv_mota.setText(yeucau);
     }
