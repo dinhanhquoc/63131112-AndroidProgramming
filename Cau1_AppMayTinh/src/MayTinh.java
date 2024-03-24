0d8f9f99a53cc67d62e9b8a9f9b4ad3254ea7898
dinhanhquoc <anhquoc004@gmail.com>
2024-03-24 15:27:53 +0700

    Add Image
    Add Image

---
+++ Cau1_AppMayTinh/src/MayTinh.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class MayTinh {
+    private JPanel panel1;
+
+    public static void main(String[] args) {
+
+    }
+}
--- ntu_63131112/Cau1_ChuongTrinhTinhBMI/src/ChuongTrinhChinh.java
+++
@@ -1,9 +0,0 @@
-
-public class ChuongTrinhChinh {
-
-	public static void main(String[] args) {
-		new ManHinhTinhToan().setVisible(true);
-		
-	}
-
-}
