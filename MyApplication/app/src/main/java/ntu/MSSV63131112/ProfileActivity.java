52d025176d9ade4e49b103ddbff1306ef2875c87
dinhanhquoc <anhquoc004@gmail.com>
2024-03-25 09:14:12 +0700

    Function Doi Man Hinh
    Function Doi Man Hinh

--- MyApplication/app/src/main/java/ntu/MSSV63131112/ProfileActivity.java
+++ MyApplication/app/src/main/java/ntu/MSSV63131112/ProfileActivity.java
@@ -1,6 +1,8 @@
 package ntu.MSSV63131112;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    Button btn_logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btn_logout = findViewById(R.id.btn_logout);
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishAffinity();
+            }
         });
     }
 }
