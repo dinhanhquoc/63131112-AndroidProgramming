df16dc9b4788a85c500be2f3023bb71dc016f491
dinhanhquoc <anhquoc004@gmail.com>
2024-03-25 10:35:00 +0700

    Create Event Switch
    Create Event Switch

--- MyApplication/app/src/main/java/ntu/MSSV63131112/ProfileActivity.java
+++ MyApplication/app/src/main/java/ntu/MSSV63131112/ProfileActivity.java
@@ -3,6 +3,8 @@ package ntu.MSSV63131112;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ProfileActivity extends AppCompatActivity {
     Button btn_logout;
+    Switch sw_color;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +26,13 @@ public class ProfileActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Đóng màn hình hiện tại
                 finishAffinity();
+            }
+        });
+        sw_color= findViewById(R.id.sw_color);
+        sw_color.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                
             }
         });
     }
